703f50ae608dadf0a8bf053bdd5e36c0884df37d
Jason Schein <jasonschein@gmail.com>
2023-05-10 22:51:39 -0700

    Some updates. Setup script. Common utils.

---
+++ setup.sh
@@ -0,0 +1,29 @@
+git_root="$(git rev-parse --show-toplevel)"
+backup_dir="${git_root}/backup"
+
+. "${git_root}/bash/bash_utils"
+
+
+sync_dotfiles_dir() {
+  syncdir="$1"
+  sync_backup="$backup_dir/$(echo "$syncdir" | sed -e "s@^$git_root/@@g" -e "s@\*@@g" -e "s@/\$@@g")"
+  mkdir -p $sync_backup
+  for f in $1
+  do
+    basen="$(basename $f)"
+    dirn="$(dirname $f)"
+    if [ -L "$HOME/.$basen" ]; then
+      echo "$(f_warn "Skipping Existing Symlink") ${dirn}/$(f_em $basen)"
+      continue
+    fi
+    if [ -f "$HOME/.$basen" ]; then
+      echo "$(f_warn Moving) $HOME/.$basen to backup dir at $backup_dir"
+      mv $HOME/.$basen $sync_backup/.$basen
+    fi
+    echo "$(f_info Linking) ${dirn}/$(f_em $basen) to $HOME/.$basen"
+    # take action on each file. $f store current file name
+    ln -s $f $HOME/.$basen
+  done
+}
+sync_dotfiles_dir "$HOME/jschein_dotfiles/bash/*"
+sync_dotfiles_dir "$HOME/jschein_dotfiles/misc/*"
